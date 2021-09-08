f0e1238a8a3a3f6f113d9a793710a18ee189d631
chaeyeon kim <kcy85078507@gmail.com>
2021-09-08 13:40:40 +0900

    2021-09-08 13:40 Js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-window.onload = function () {
-  function onClick() {
-    document.querySelector(".sidenav").style.display = "block";
-    document.querySelector(".black_bg").style.display = "block";
-    document.querySelector("body").style.overflow = "hidden";
-    document.querySelector(".store__categ").style.visibility = "hidden";
-  }
-  function offClick() {
-    document.querySelector(".sidenav").style.display = "none";
-    document.querySelector(".black_bg").style.display = "none";
-    document.querySelector("body").style.removeProperty("overflow");
-    document.querySelector(".store__categ").style.removeProprty("visibility");
-  }
-  document
-    .querySelector(".navbar__toggleBtn")
-    .addEventListener("click", onClick);
-  document.querySelector(".cancel").addEventListener("click", offClick);
-};
+document
+  .querySelector(".navbar__toggleBtn")
+  .addEventListener("click", navClickOn);
+document.querySelector(".cancel").addEventListener("click", navClickOff);
+document.querySelector(".question").addEventListener("click", guideClickOn);
+document.querySelector(".question").addEventListener("click", guideClickOff);
 
-// window.onload = function () {
-//   function onClick() {
-//     document.querySelector(".answer").style.display = "block";
-//   }
-//   function offClick() {
-//     document.querySelector(".answer").style.display = "none";
-//   }
-//   document.querySelector(".question").addEventListener("click", onClick);
-//   document.querySelector(".question").addEventListener("click", offClick);
-// };
+function navClickOn() {
+  document.querySelector(".sidenav").style.display = "block";
+  document.querySelector(".black_bg").style.display = "block";
+  document.querySelector("body").style.overflow = "hidden";
+  document.querySelector(".store__categ").style.visibility = "hidden";
+}
+function navClickOff() {
+  document.querySelector(".sidenav").style.display = "none";
+  document.querySelector(".black_bg").style.display = "none";
+  document.querySelector("body").style.removeProperty("overflow");
+  document.querySelector(".store__categ").style.removeProprty("visibility");
+}
+function guideClickOn() {
+  document.querySelector(".answer").style.display = "block";
+}
+function guideClickOff() {
+  document.querySelector(".answer").style.display = "none";
+}
