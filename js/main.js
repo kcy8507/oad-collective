d13e6f1bc6ec4788c91c473731e7cbd25028a1d6
chaeyeon kim <kcy85078507@gmail.com>
2021-09-08 14:31:27 +0900

    2021-09-08 14:31 FIN

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ document
   .querySelector(".navbar__toggleBtn")
   .addEventListener("click", navClickOn);
 document.querySelector(".cancel").addEventListener("click", navClickOff);
-document.querySelector(".question").addEventListener("click", guideClickOn);
-document.querySelector(".question").addEventListener("click", guideClickOff);
+// document.querySelector(".question").addEventListener("click", guideClickOn);
+// document.querySelector(".question").addEventListener("click", guideClickOff);
 
 function navClickOn() {
   document.querySelector(".sidenav").style.display = "block";
@@ -17,9 +17,20 @@ function navClickOff() {
   document.querySelector("body").style.removeProperty("overflow");
   document.querySelector(".store__categ").style.removeProprty("visibility");
 }
-function guideClickOn() {
-  document.querySelector(".answer").style.display = "block";
-}
-function guideClickOff() {
-  document.querySelector(".answer").style.display = "none";
-}
+// function guideClickOn() {
+//   document.querySelector(".answer").style.display = "block";
+// }
+// function guideClickOff() {
+//   document.querySelector(".answer").style.display = "none";
+// }
+
+var questionArray = document.querySelectorAll(".question");
+var answerArray = document.querySelectorAll(".answer");
+console.log(answerArray);
+document.querySelectorAll(".question").forEach(function (i) {
+  i.addEventListener("click", function (e) {
+    console.log(e.target);
+    const clickedQuestion = [...questionArray].indexOf(e.target);
+    answerArray[clickedQuestion].classList.toggle("hidden");
+  });
+});
